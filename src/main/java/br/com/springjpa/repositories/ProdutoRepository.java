adec600a98310389778582a16056be33f66a808b
Daniel Henrique Cavalcante da Silva_ <daniel.hsilva@totvs.com.br>
2022-12-30 03:16:23 -0300

    Edit ProdutoRepository

--- src/main/java/br/com/springjpa/repositories/ProdutoRepository.java
+++ src/main/java/br/com/springjpa/repositories/ProdutoRepository.java
@@ -1,5 +1,6 @@
 package br.com.springjpa.repositories;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,7 +12,7 @@ import br.com.springjpa.model.Produto;
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 
 	@Query("SELECT p FROM produtos p WHERE p.preco > :P_PRECO")
-	public List<Produto> findProdutosPrecoMaior(@Param("P_PRECO") String preco);
+	public List<Produto> findProdutosPrecoMaior(@Param("P_PRECO") BigDecimal preco);
 
 	@Query(value = "SELECT * FROM PRODUTOS WHERE LIKE DESCRICAO LIKE :P_DESCRICAO%", nativeQuery = true)
 	public List<Produto> findByDescricao(@Param("P_DESCRICAO") String descricao);
