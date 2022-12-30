ecc6d0bc0a786ed760a1c32fc400a8799f59fb69
Daniel Henrique Cavalcante da Silva_ <daniel.hsilva@totvs.com.br>
2022-12-30 03:03:09 -0300

    Criando ProdutoRepository

---
+++ src/main/java/br/com/springjpa/repositories/ProdutoRepository.java
@@ -0,0 +1,21 @@
+package br.com.springjpa.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import br.com.springjpa.model.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+	@Query("SELECT p FROM produtos p WHERE p.preco > :P_PRECO")
+	public List<Produto> findProdutosPrecoMaior(@Param("P_PRECO") String preco);
+
+	@Query(value = "SELECT * FROM PRODUTOS WHERE LIKE DESCRICAO LIKE :P_DESCRICAO%", nativeQuery = true)
+	public List<Produto> findByDescricao(@Param("P_DESCRICAO") String descricao);
+
+	public List<Produto> findByNomeLike(String nome);
+
+}
