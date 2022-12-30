8f2732483738750896c4ec51bbd9734a461c0d39
Daniel Henrique Cavalcante da Silva_ <daniel.hsilva@totvs.com.br>
2022-12-30 03:35:46 -0300

    Criando ProdutoService

---
+++ src/main/java/br/com/springjpa/services/ProdutoServices.java
@@ -0,0 +1,71 @@
+package br.com.springjpa.services;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.springjpa.model.Produto;
+import br.com.springjpa.repositories.ProdutoRepository;
+
+@Service
+public class ProdutoServices {
+
+	@Autowired
+	private ProdutoRepository repository;
+
+	public Optional<Produto> findById(Long id) {
+		return this.repository.findById(id);
+	}
+
+	public List<Produto> findAll() {
+		return this.repository.findAll();
+	}
+
+	public List<Produto> findProdutosPrecoMaior(BigDecimal preco) {
+		return this.repository.findProdutosPrecoMaior(preco);
+	}
+
+	public List<Produto> findByDescricao(String descricao) {
+		return this.repository.findByDescricao(descricao);
+	}
+
+	public Produto insert(Produto produto) {
+		try {
+			this.repository.save(produto);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return produto;
+	}
+
+	public Produto delete(Produto produto) {
+		try {
+			Optional<Produto> optProduto = this.findById(produto.getId());
+			
+			if (optProduto.isPresent()) {
+				Produto entity = optProduto.get();
+				this.repository.delete(entity);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return produto;
+	}
+
+	public Produto update(Produto produto) {
+		try {
+			Optional<Produto> optProduto = this.findById(produto.getId());
+			
+			if (optProduto.isPresent()) {
+				this.repository.save(produto);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return produto;
+	}
+
+}
